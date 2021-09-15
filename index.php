bcf96030216160d39470aacb293f0fae2ee7fe55
Mukul <sr.mukul9090@gmail.com>
2021-09-15 19:09:56 +0600

    bogarithm blogsite navbar build

--- index.php
+++ index.php
@@ -7,15 +7,40 @@
     <!-- webpage title -->
     <title>Bogarithm / Home</title>
     <!-- external CSS -->
-    <link rel="stylesheet" href="./src/style/style.css">
+    <link rel="stylesheet" href="./src/style/sass/style.css">
     <!-- favicon -->
     <link rel="shortcut icon" href="./public/favicon.ico" type="image/x-icon">
+    <!-- unicons -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/thinline.css">
 </head>
 <body>
     <!-- header section -->
     <header id="header-section">
-        <div class="container">
-            <!--  -->
+        <div class="header-container">
+            <!-- branding -->
+            <div class="brand-info">
+                <a href="./index.php">
+                    <h1 class="brand-title">Bogarithm</h1>
+                </a>
+            </div>
+
+            <!-- navbar toggle button -->
+            <div class="navbar-toggle">
+                <button id="btn-navbar-toggle">
+                    <i class="uit uit-align-center-alt"></i>
+                </button>
+            </div>
+
+            <!-- navbar -->
+            <nav class="header-navbar">
+                <ul>
+                    <li><a href="./index.php" class="active">Home</a></li>
+                    <li><a href="./src/pages/trending.php">trending</a></li>
+                    <li><a href="./src/pages/explore.php">Explore Us</a></li>
+                    <li><a href="./src/pages/registration.php">Registration</a></li>
+                    <li><a href="./src/pages/login.php">Login</a></li>
+                </ul>
+            </nav>
         </div>
     </header>
 
