2a1bd66b7b3d4e5cc79e655ec35fff34489f8d5b
Mukul <sr.mukul9090@gmail.com>
2021-09-19 15:24:11 +0600

    project boilerplate file re-initialize

--- index.php
+++ index.php
@@ -11,40 +11,16 @@
     <!-- favicon -->
     <link rel="shortcut icon" href="./public/favicon.ico" type="image/x-icon">
     <!-- unicons -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/thinline.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 </head>
 <body>
-    <!-- header section -->
-    <header id="header-section">
-        <div class="header-container">
-            <!-- branding -->
-            <div class="brand-info">
-                <a href="./index.php">
-                    <h1 class="brand-title">Bogarithm</h1>
-                </a>
-            </div>
+    <!-- ========= HEADER START ========= -->
+    <!-- ========= HEADER END ========= -->
 
-            <!-- navbar toggle button -->
-            <div class="navbar-toggle">
-                <button id="btn-navbar-toggle">
-                    <i class="uit uit-align-center-alt"></i>
-                </button>
-            </div>
+    <!-- ========= MAIN / BODY START ========= -->
+    <!-- ========= MAIN / BODY START ========= -->
 
-            <!-- navbar -->
-            <nav class="header-navbar">
-                <ul>
-                    <li><a href="./index.php" class="active">Home</a></li>
-                    <li><a href="./src/pages/trending.php">trending</a></li>
-                    <li><a href="./src/pages/explore.php">Explore Us</a></li>
-                    <li><a href="./src/pages/registration.php">Registration</a></li>
-                    <li><a href="./src/pages/login.php">Login</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
-    <!-- main section -->
-    <!-- footer section -->
+    <!-- ========= FOOTER START ========= -->
+    <!-- ========= FOOTER END ========= -->
 </body>
 </html>
